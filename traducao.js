cd849e08a9cd72a777d98ca036c9edc618fa0e7f
decobot <capy@deco.cx>
2024-11-08 16:53:21 -0300

    translate button

---
+++ traducao.js
@@ -0,0 +1,42 @@
+function translatePage(targetLanguage) {
+    const apiKey = 'AIzaSyAPXbbPo89J9jame2kubA22u3TxcBzBWUI'; // Substitua pela sua chave de API do Google Translate
+  
+    $('h1, h2, h3, h4, h5, p, li, span, div, a').each(function () {
+      const element = $(this);
+      if (element.text().trim() !== "" && !element.children().length) {
+        const originalHTML = element.html();
+        const text = element.text().trim();
+  
+        $.ajax({
+          url: `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`,
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            q: text,
+            target: targetLanguage,
+            format: 'text'
+          }),
+          success: function (response) {
+            const translatedText = response.data.translations[0].translatedText;
+            element.html(originalHTML.replace(text, translatedText));
+          },
+          error: function (err) {
+            console.error('Erro na tradução', err);
+          }
+        });
+      }
+    });
+  }
+  
+  $(document).ready(function () {
+    $('#translateButton').click(function () {
+      translatePage('en');
+    });
+    $('#translateButton2').click(function () {
+      translatePage('es');
+    });
+    $('#translateButton3').click(function () {
+      translatePage('pt-BR');
+    });
+  });
+  
