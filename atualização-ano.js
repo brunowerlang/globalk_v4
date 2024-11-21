c7a459d1420f82585c794f670ba25d7f3bd77214
decobot <capy@deco.cx>
2024-11-21 16:51:44 -0300

    att

---
+++ atualização-ano.js
@@ -0,0 +1,5 @@
+ // Obtém o ano atual
+ const currentYear = new Date().getFullYear();
+  
+ // Atualiza o conteúdo do elemento com o ID "current-year"
+ document.getElementById('current-year').textContent = currentYear;
